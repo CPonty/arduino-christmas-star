82acbb8d9a5447ee211ee295be81d4ca4ff5688c
CPonty <christopher.ponticello@uqconnect.edu.au>
2014-01-12 01:08:34 +1000

    Reached a state worthy of demoing.
    All lighting modes implemented. Reshuffled global variables; added a
    bunch of utilities. Serial input polled whenever a delay period starts
    or the lights change state.

--- christmasStar.ino
+++ christmasStar.ino
@@ -81,24 +81,33 @@ void stateFadeTo(char bitmask, int tinterval);
 void runPattern(char * pattern, int tinterval, bool fade);
 bool runPatternId(char patternId, int tinterval, bool fade);
 bool runModeId(char modeId);
-void modeOff();
-void modeOn();
-void modePulse();
-void modeSequential();
-void modeRandom();
-void modeFixed();
-void err(const char * msg);
+void modeOff(void);
+void modeOn(void);
+void modePulse(void);
+void modeSequential(void);
+void modeRandom(void);
+void modeFixed(void);
+void msg(const char * message);
+void err(const char * message);
+int arrayLen(char * arr);
+bool serialHandle(void);
+void block(unsigned long ms);
+bool modeChanged(void);
 
 //-----------------------------------------------------------------------------
 
-#define TEST
+// #define TEST
+#define PRINT
 
 #define nLEDs 6 // 6 LEDs connected to the Arduino
-#define nCYCLES nLEDs*4; // indicates how many states a pattern should loop through
+#define nCYCLES nLEDs*4 // indicates #states a pattern should loop through
 #define TFADE 8 // delay between LED fade levels (ms)
 
+char pins_gnd[nLEDs +1] = {13,12,8,7,4,2, -1};
+char pins_pwm[nLEDs +1] = {10,11,6,9,3,5, -1};
+
 #define BPM_MIN 1 // "beats per minute" for PULSE mode (user can set on console)
-#define BPM_DEFAULT 60
+#define BPM_DEFAULT 220
 #define BPM_MAX 1000
 
 #define MODE_OFF 0
@@ -110,14 +119,24 @@ void err(const char * msg);
 #define MODE_DEFAULT MODE_RANDOM
 #define MODE_MAX 5
 
-char pins_gnd[nLEDs +1] = {13,12,8,7,4,2, -1};
-char pins_pwm[nLEDs +1] = {10,11,6,9,3,5, -1};
+void (*modes[MODE_MAX +2])(void) = {modeOff, modeOn, modePulse, modeSequential,
+		 					 		modeRandom, modeFixed, NULL};
+									
+#define PATTERN_TEST 0
+#define PATTERN_ON 1
+#define PATTERN_FLASH 2
+#define PATTERN_ROTATE 3
+#define PATTERN_COUNT 4
+#define PATTERN_MEXICAN 5
+#define PATTERN_TRIANGLE 6
+#define PATTERN_DEFAULT PATTERN_TEST
+#define PATTERN_MAX 6
 
 // Bitmasks of 0b00xxxxxx, where the lower 6 bits indicate the active LEDs
 // Signed value; negative value (MSB=1) indicates end-of-array
 char pattern_test[nLEDs +1] = {0x20, 0x10, 0x08, 0x04, 0x02, 0x01, -1};
-char pattern_error[2 +1] = {0x2A, 0x00, -1};
-char pattern_off[1 +1] = {0x00, -1};
+char pattern_error[2 +1] = {0x2A, 0x00, -1};//only for err(), no #def ID
+char pattern_off[1 +1] = {0x00, -1}; 		//only for modeOff(), no #def ID
 char pattern_on[1 +1] = {0x3F, -1};
 char pattern_flash[2 +1] = {0x3F, 0x00, -1};
 char pattern_rotate[3 +1] = {0x24, 0x12, 0x09, -1};
@@ -126,24 +145,14 @@ char pattern_count[nLEDs*2 +1] = {0x00, 0x20, 0x30, 0x38, 0x3C, 0x3E, 0x3F,
 char pattern_mexican[nLEDs +1] = {0x38, 0x1C, 0x0E, 0x07, 0x23, 0x31, -1};
 char pattern_triangle[2 +1] = {0x2A, 0x15, -1};
 
- char * patterns[7 +1] = {pattern_test, /*pattern_off,*/ /*pattern_error,*/
+char * patterns[PATTERN_MAX +2] = {pattern_test, /*error, off, */
 	 	pattern_on, pattern_flash, pattern_rotate, pattern_count,
 		pattern_mexican, pattern_triangle, NULL};
-		
-#define PATTERN_TEST 0
-#define PATTERN_ON 1
-#define PATTERN_FLASH 2
-#define PATTERN_ROTATE 3
-#define PATTERN_COUNT 4
-#define PATTERN_MEXICAN 5
-#define PATTERN_TRIANGLE 6
-#define PATTERN_DEFAULT PATTERN_FLASH
-#define PATTERN_MAX 6
 
 #define TDELAY_MIN 64 // LED state time period (ms)
 #define TDELAY_DEFAULT 256
 #define TDELAY_MAX 1024
-#define TDELAY_SET_MIN 1 // (user sets value 1..5, scales to 64*(2**(value-1)))
+#define TDELAY_SET_MIN 1 // (user sets value 1..5, scales to 64*(2**(val-1)))
 #define TDELAY_SET_MAX 5
 		
 #define FADE_DEFAULT true //toggles smoothly fading between LED states
@@ -161,6 +170,8 @@ int bpm = BPM_DEFAULT;
 char pattern = PATTERN_DEFAULT;
 
 char mode = MODE_DEFAULT; 
+
+bool modeChange = false; // flag set by serialHandle to trigger mode changing
 
 //-----------------------------------------------------------------------------
 
@@ -241,6 +252,7 @@ void stateFadeTo(char bitmask, int tinterval)
  */
 void runPattern(char * pattern, int tinterval, bool fade) 
 {
+	if (serialHandle()) return;
 	for (char *p=pattern; *p>-1; p++) {
 		if (fade) {
 			stateFadeTo(*p, tinterval);
@@ -248,6 +260,7 @@ void runPattern(char * pattern, int tinterval, bool fade)
 			stateSet(*p);
 			delay(tinterval);
 		}
+		if (serialHandle()) return;
 	}
 	//stateSet(0);
 }
@@ -272,23 +285,161 @@ bool runPatternId(char patternId, int tinterval, bool fade)
  */
 bool runModeId(char modeId) 
 {
-	//TODO
+	if (modeId<0 || modeId>MODE_MAX)
+		return false;
+	(*modes[modeId])();
+	return true;
 }
 
-//TODO string to int
-//TODO set____() functions for each applicable global
+/* msg(message)
+ *  action: print a serial MESSAGE.
+ *          If MESSAGE is null, nothing is printed.
+ */
+void msg(const char * message) 
+{
+#ifdef PRINT
+	if (message != NULL)
+		Serial.println(message);
+#endif
+}
 
-/* err()
+/* err(message)
  *  action: print a serial MESSAGE, run pattern_error() and halt. 
  *          User sees blinking red lights.
  *          If MESSAGE is null, nothing is printed.
  */
 void err(const char * message) 
 {
-	if (message != NULL) {
+#ifdef PRINT
+	if (message != NULL)
 		Serial.println(message);
+#endif
+	while(1) 
+		runPattern(pattern_error, 256, true);
+}
+
+//-----------------------------------------------------------------------------
+
+/* arrayLen(arr)
+ *   action: return the length of a negative-terminated array of characters
+ *   preconditions: block of bytes at ARR is negative-terminated
+ */
+int arrayLen(char * arr)
+{
+	int ret;
+	for (ret=0; arr[ret]>=0; ret++);
+	return ret;
+}
+
+//TODO string to int
+//TODO set____() functions for each applicable global
+
+//TODO write it properly, this just acknowledges a character.
+bool serialHandle(void) 
+{
+    while (Serial.available() > 0) {
+		int incomingByte = Serial.read();
+		Serial.print("I received: ");
+        Serial.println(incomingByte, DEC);
+    }
+}
+
+/* block(ms)
+ *   action: wait for MS milliseconds. Handle serial connections afterwards
+ */
+void block(unsigned long ms) 
+{
+	delay(ms);
+	serialHandle();
+}
+
+/* modeChanged()
+ *   action: if modeChange flag is set, clear it and return true.
+ */
+bool modeChanged(void)
+{
+	if (modeChange) {
+		modeChange = false;
+		return true;
 	}
-	while(1) runPattern(pattern_error, 256, true);
+	return false;
+}
+
+//-----------------------------------------------------------------------------
+
+void modeOff(void) /* 0 */
+{
+	msg(" > MODE: 0 (Off)");
+	while(1) {
+		runPattern(pattern_off,64,false);
+		if (modeChanged()) return;
+	}
+}
+
+void modeOn(void) /* 1 */
+{
+	msg(" > MODE: 1 (On)");
+	while (1) {
+		runPattern(pattern_on,64,false);
+		if (modeChanged()) return;
+	}
+}
+
+void modePulse(void) /* 2 */
+{
+	msg(" > MODE: 2 (Pulsating)");
+	while (1) {
+		runPattern(pattern_flash, ((unsigned long)1000*60)/bpm, fade);
+		if (modeChanged()) return;
+	}
+}
+
+void modeSequential(void) /* 3 */
+{
+	msg(" > MODE: 3 (Sequential Patterns)");
+	while (1) {
+	    for (char **p=patterns; *p!=NULL; p++) {
+			// Calculate the #repeats. 
+			//  equivalent to ceil(totalStates/#patternStates)
+			int nrepeats = ((nCYCLES + arrayLen(*p) -1) / arrayLen(*p));
+			for (; nrepeats>0; nrepeats--) {
+				runPattern(*p, tdelay, true);
+				if (modeChanged()) return;
+			}
+	  		block(tdelay);
+			if (modeChanged()) return;
+	    }
+	}
+}
+
+void modeRandom(void) /* 4 */
+{
+	msg(" > MODE: 4 (Random Patterns)");
+	while (1) {
+		// Locally randomized parameters (time, #repeats, pattern, fade).
+		//  We don't want the randomizer's selections to overwrite globals
+		//  set by defaults / console input.
+		int ttime = TDELAY_MIN * pow(2,random(TDELAY_SET_MIN-1, TDELAY_SET_MAX-1));
+		char patternId = random(0, PATTERN_MAX +1);
+		int patLen = arrayLen(patterns[patternId]);
+		int nrepeats = ((random(nCYCLES/2, nCYCLES) + patLen -1) / patLen);
+		bool doFade = (bool)random(0, 1 +1);
+		for (; nrepeats>0; nrepeats--) {
+			runPatternId(patternId, ttime, doFade);
+			if (modeChanged()) return;
+		}
+  		block(tdelay);
+		if (modeChanged()) return;
+	}
+}
+
+void modeFixed(void) /* 5 */
+{
+	msg(" > MODE: 5 (Fixed Pattern)");
+	while (1) {
+		runPatternId(pattern, tdelay, fade);
+		if (modeChanged()) return;
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -303,7 +454,7 @@ void setup()
   
   // serial setup
   Serial.begin(9600);
-  Serial.println("\n ~~ Chris Ponticello's Arduino Christmas Star ~~\n");
+  msg("\n ~~ Chris Ponticello's Arduino Christmas Star ~~\n");
   
 #ifdef TEST
   //A :: fixed test, just lighting sequence 1-6 
@@ -323,7 +474,7 @@ void loop()
   }
 #else
   
-  err(NULL);
+  //err("Error: no error. :P");
   runModeId(mode);
 	
 #endif
@@ -337,10 +488,9 @@ void loop()
 		X- make a function for executing a given pattern at a given speed
 		X- make a function for executing a given pattern with fade
 		X- extend err() to print to the console
-		- implement all the modes
-			> higher-level functions for executing patterns
-			> modes: sequential, random, pulse only, on only
-		- implement all the commands console commands
+		X- implement all the modes
+		X	> higher-level functions for executing patterns
+		X	> modes: off, on, pulse, sequential, random, fixed pattern
 		- add a parallel serial console line reader
 		- add a parser to the serial console to recognise commands:
 			mode ___ (0=off, 1=on, 2=pulse, 3=sequential, 4=random, 5=fixed)
@@ -349,7 +499,14 @@ void loop()
 			speed ___ (1..5, scales to 64,128,256,512,1024)
 			pattern ___ ()
 			help (print usable commands)
+		- implement all the commands console commands
+		- ensure error hooks & "restart" hooks are correct
 
+	Later:
+		- simplify - combine "pulse" and "fixed", such that "fixed" starts on
+					  the pulsing pattern. Enables BPM to be set for any pattern.
+
+	//
 	problem of parallel console & operations:
 		- every time delay(x) is called, instead call block(x)
 		- block is a custom function which does serial_handle() and then delay(x)
